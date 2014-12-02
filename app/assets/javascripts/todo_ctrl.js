cdd58f72e20806d4d7503f923e381f1083afac80
Jay Wengrow <jaywngrw@gmail.com>
2014-12-01 19:18:26 -0600

    another implementation of solution

--- app/assets/javascripts/todo_ctrl.js
+++ app/assets/javascripts/todo_ctrl.js
@@ -2,7 +2,45 @@
   "use strict";
 
   angular.module("app").controller("todoCtrl", function($scope){
-    
+    $scope.tasks = [
+      { name: "Take out trash",
+        completed: false,
+      },
+      {
+        name: "Feed the cat",
+        completed: false
+      }
+    ];
+
+    $scope.addTask = function(task) {
+      $scope.tasks.push({ name: task, completed: false });
+      $scope.newTask = "";
+    };
+
+    $scope.toggleComplete = function(task) {
+      task.completed = !task.completed;
+    };
+
+    $scope.numberOfIncompleteTasks = function() {
+      var count = 0;
+      for(var i = 0; i < $scope.tasks.length; i++) {
+        if(!$scope.tasks[i].completed) {
+          count++;
+        }
+      }
+      return count;
+    }
+
+    $scope.deleteCompletedTasks = function() {
+      for(var i = 0; i < $scope.tasks.length; i++) {
+        if($scope.tasks[i].completed) {
+          $scope.tasks.splice(i, 1);
+        }
+      }
+    };
+
+    window.scope = $scope;
+
   });
 
 }());
