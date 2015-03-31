f391efe1138bae1616d3f45ff9a947d249438c4a
Jay Wengrow <jaywngrw@gmail.com>
2015-03-30 19:29:10 -0500

    preparing for demo

--- app/assets/javascripts/todo_ctrl.js
+++ app/assets/javascripts/todo_ctrl.js
@@ -10,34 +10,6 @@
       $scope.newTask = "";
     };
 
-    $scope.toggleTask = function(task) {
-      task.completed = !task.completed;
-    };
-
-    $scope.numberOfIncompleteTasks = function(task) {
-      var count = 0;
-      for(var i = 0; i < $scope.tasks.length; i++) {
-        if(!$scope.tasks[i].completed) {
-          count += 1;
-        }
-      }
-      return count;
-    };
-
-    $scope.removeCompletedTasks = function() {
-      var completedTasks = [];
-
-      for(var i = 0; i < $scope.tasks.length; i++) {
-        if($scope.tasks[i].completed) {
-          completedTasks.push($scope.tasks[i]);
-        }
-      }
-        
-      for(var i = 0; i < completedTasks.length; i++) {
-        $scope.tasks.splice($scope.tasks.indexOf(completedTasks[i]), 1);
-      }
-    };
-
     window.scope = $scope;
   });
 
